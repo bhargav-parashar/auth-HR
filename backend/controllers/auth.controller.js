b39d538321f410c83ef6c586e13f6561d8888317
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-03-13 00:13:48 +0530

    feat: Register user backend api endpoint build

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,23 +3,80 @@ const AuthServiceInstance = new AuthService();
 const UserService = require("../services/user.service");
 const UserServiceInstance = new UserService();
 
-
 //CREATE USER
-const register = async (req,res) =>{
-    try{
-        //Hash Password
-        const hashedPassword = await AuthServiceInstance.generatePasswordHash(req.body.password);
-        const newUser = await UserServiceInstance.create({...req.body, password : hashedPassword});
-        const newUserRole = await UserServiceInstance.createUserRole({userId:newUser._id});
-        res.status(201).json({message: "User registered successfully"});
-     }catch(err){
-        res.status(500).send({ message: "Registration failed!", err });
-    }
+const register = async (req, res) => {
+  try {
+    //Hash Password
+    const hashedPassword = await AuthServiceInstance.generatePasswordHash(
+      req.body.password
+    );
+    const newUser = await UserServiceInstance.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+    const newUserRole = await UserServiceInstance.createUserRole({
+      userId: newUser._id,
+    });
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).send({ message: "Registration failed!", err });
+  }
 };
 
+const login = async (req, res) => {
+  try {
+    //search users table by username
+    const user = await UserServiceInstance.findByUsername(req.body.username);
 
+    if (!user) {
+      res
+        .status(401)
+        .json({ message: "Either username or password is incorrect" });
+    } else {
+      //if found match password / jwt compare
+      const isPasswordValid = await AuthServiceInstance.comparePasswordHash(
+        req.body.password,
+        user.password
+      );
+     
+      //if matched create jwt token
+      if (isPasswordValid) {
+        //get user role
+        const { roleId } = await UserServiceInstance.getUserRoleMapping(
+          user._id
+        );
+        
+        const { role } = await UserServiceInstance.getRole(roleId);
+       
+        //create jwt token
+        const jwtToken =  AuthServiceInstance.generateJwt({
+          userId: user._id,
+        });
+        
+        res
+          .status(200)
+          .cookie("remember-token", jwtToken, {
+            maxAge: 30 * 60 * 1000,
+            httpOnly: true,
+            secure: true,
+            sameSite: "None",
+          })
+          .json({ 
+                  token: jwtToken, 
+                  role: role 
+          });
+      } else {
+        res.status(401).json({ message: "Either username or password is incorrect" });
+      }
+    }
 
+    //return jwt token
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong!", err });
+  }
+};
 
 module.exports = {
-    register
+  register,
+  login,
 };
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const {register} = require("../controllers/auth.controller");
+const {register, login} = require("../controllers/auth.controller");
 const {validateSchema} = require("../middlewares/validate.middleware");
 const {authValidationSchema} = require("../validations/auth.validations");
 
 router.post("/register", validateSchema(authValidationSchema), register);
+router.post("/login", validateSchema(authValidationSchema), login);
 
 module.exports = router;
 
